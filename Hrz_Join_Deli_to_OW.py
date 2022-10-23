da2510892ea7ad9d2555c899257442b88d3b52ee
saulocpp <82084498+saulocpp@users.noreply.github.com>
2022-10-23 22:11:53 +0200

    Add files via upload

--- Hrz_Join_Deli_to_OW.py
+++ Hrz_Join_Deli_to_OW.py
@@ -1,6 +1,5 @@
-# 12/05/2022
-# Python script to join multiple IL,XL,X,Y,Z horizon files from Deli into one OW horizon file for loading (based on the Landmark horizon default format in Data Export)
-# Written by Saulo Silva - saulocpp@gmail.com
+# Horizon names are limited to 60 characters as per the format file, so file names will be internally trimmed at the end
+# Use file format HorizonExportv1p0_3d.afm.xml in Data Import to load
 
 import os
 import sys
@@ -11,7 +10,7 @@ from pyarrow import csv
 from random import randint
 from datetime import datetime
 
-def hrz_deli2ow(file_list, interp, survey, domain, onset, attribute):
+def hrz_deli2ow(file_list, interp, survey, domain, onset):
 	with open("Deli2OW_" + datetime.now().strftime("%d-%m-%Y_%H%M%S") + ".dat", "w") as d2ow:
 		for file_name in file_list:
 			readoptions		= csv.ReadOptions(autogenerate_column_names = True)
@@ -20,7 +19,7 @@ def hrz_deli2ow(file_list, interp, survey, domain, onset, attribute):
 			LINE_COUNT		= deli.num_rows
 			INTERP_NAME		= interp + " " * (9 - len(interp))
 			DOMAIN			= domain + " " * (10 if len(domain) == 5 else 11)
-			ATTRIBUTE		= attribute + " " * (40 - len(attribute))
+			ATTRIBUTE		= domain + "_STRUCTURE" + " " * (25 if len(domain) == 5 else 26)
 			HRZ_NAME		= os.path.basename(os.path.splitext(file_name)[0])
 			HRZ_NAME		= HRZ_NAME[:60] if len(HRZ_NAME) > 60 else HRZ_NAME + " " * (60 - len(HRZ_NAME))
 			HRZ_ONSET		= onset + " " * (17 - len(onset))
@@ -31,11 +30,13 @@ def hrz_deli2ow(file_list, interp, survey, domain, onset, attribute):
 			print(INTERP_NAME + DOMAIN + ATTRIBUTE + HRZ_NAME + HRZ_ONSET + HRZ_RGB, file = d2ow)
 			print(HRZ_VERSION + SURVEY_NAME, file = d2ow)
 			for i in range(1, LINE_COUNT):
-				ILINE = str(deli["f0"][i]) + " " * (16 - len(str(deli["f0"][i])))
-				XLINE = str(deli["f1"][i]) + " " * (16 - len(str(deli["f1"][i])))
-				VAL_X = str(deli["f2"][i]) + " " * (16 - len(str(deli["f2"][i])))
-				VAL_Y = str(deli["f3"][i]) + " " * (16 - len(str(deli["f3"][i])))
-				VAL_Z = str(deli["f4"][i]) + " " * (16 - len(str(deli["f4"][i])))
-				print(ILINE, XLINE, VAL_X, VAL_Y, VAL_Z, file = d2ow)
+				if(str(deli["f2"][i]) != "nan" and str(deli["f3"][i]) != "nan"):
+					ILINE = str(deli["f0"][i]) + " " * (16 - len(str(deli["f0"][i])))
+					XLINE = str(deli["f1"][i]) + " " * (16 - len(str(deli["f1"][i])))
+					VAL_X = str(deli["f2"][i]) + " " * (16 - len(str(deli["f2"][i])))
+					VAL_Y = str(deli["f3"][i]) + " " * (16 - len(str(deli["f3"][i])))
+					VAL_Z = str(deli["f4"][i]) + " " * (16 - len(str(deli["f4"][i])))
+					print(ILINE, XLINE, VAL_X, VAL_Y, VAL_Z, file = d2ow)
 	d2ow.close()
 	tk.messagebox.showinfo("Deli to OW conversion finished", "Use the format file 'HorizonExportv1p0.afm.xml' in Data Import to load the result in OpenWorks.")
+
